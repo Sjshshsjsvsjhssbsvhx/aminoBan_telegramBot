67f28edc1a16ec1a05df65dedb19994b0e74d413
Ririkari <80419570+Ririkari@users.noreply.github.com>
2022-01-08 16:37:03 +0300

    fixed some bugs

--- app.py
+++ app.py
@@ -7,12 +7,13 @@ token: str = str()
 #init telegram bot config file
 if path.isfile("conf.json") == True:
     is_true_token: str = str()
-    while (is_true_token != "Y") or (is_true_token != "N"):
+    while True:
         is_true_token = str(input('Start bot with current token: "Y"/"N": ')).upper()
         if is_true_token == "Y":
             with open("conf.json", "r", encoding= "utf-8") as File:
                 data: dict = json.load(File)
                 token = data.get("token")
+            break
         elif is_true_token == "N":
             token = input("Telegram bot TOKEN: ")
             with open("conf.json", "w", encoding= "utf-8") as File:
@@ -20,6 +21,7 @@ if path.isfile("conf.json") == True:
                             {"token": token},
                             File
                         )
+            break
 else:
     token = input("Telegram bot TOKEN: ")
     with open("conf.json", "w", encoding= "utf-8") as File:
@@ -40,34 +42,34 @@ def init_bot(message) -> None:
 @bot.message_handler(commands= 'help')
 def help_command(message) -> None:
     bot.reply_to(message, '''
-        Забанить участника: ban (link) (причина)\n
-        Разбанить участника: unban (link) (причина)\n\n
+        Забанить участника: /ban (link) (причина)\n
+        Разбанить участника: /unban (link) (причина)\n\n
         Пример:\n
-        ban http://aminoapps.com/p/tscygh пидор\n
-        unban http://aminoapps.com/p/tscygh уже не пидор\n\n
+        /ban http://aminoapps.com/p/tscygh пидор\n
+        /unban http://aminoapps.com/p/tscygh уже не пидор\n\n
         Наличие причины не обязательно. Пример:\n
-        ban http://aminoapps.com/p/tscygh\n
-        unban http://aminoapps.com/p/tscygh
+        /ban http://aminoapps.com/p/tscygh\n
+        /unban http://aminoapps.com/p/tscygh
     ''')
 
 #echo
-@bot.message_handled(func= lambda message: True)
+@bot.message_handler(commands= ['ban', 'unban'])
 def echo(message) -> None:
     text: str = message.text
     command: list[str] = text.split()
     if (len(command) == 2) or (len(command) == 3):
         link: str = command[1]
-        reason: str = str()
+        reason: str = "Забанен"
         result: bool = bool()
         if len(command) == 3:
             reason: str = command[2]
-        if command[0] == "ban":
+        if command[0] == "/ban":
             result = amino.ban(link, reason)
-        elif command[0] == "unban":
+        elif command[0] == "/unban":
             result = amino.unban(link, reason)
         if result == True:
-            bot.reply_to(message, "Успешно")
+            bot.reply_to(message, "Успешно\n@Smugiepop\n@Tonyous")
         elif result == False:
-            bot.reply_to(message, "Провал")
+            bot.reply_to(message, "Провал\n@Smugiepop\nTonyous")
 
 bot.infinity_polling()
